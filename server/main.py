e64731c70e8d546cdb360a9eaebaaa7f648f02c0
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2025-11-25 19:16:32 +0530

    [added controller support]

--- server/main.py
+++ server/main.py
@@ -2,27 +2,27 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
-# from pynput.keyboard import Controller
+from pynput.keyboard import Controller
 import time
 import io
 import qrcode
 
-# keyboard = Controller()
+keyboard = Controller()
 
 app = FastAPI()
 app.mount("/client", StaticFiles(directory="client"), name="client")
 
-origins = ["http://localhost:8000"]
-# origins = ["http://192.168.0.100:8000"]
+# origins = ["http://localhost:8000"]
+origins = ["http://192.168.0.100:8000"]
 app.add_middleware(CORSMiddleware, allow_origins=origins)
 
 # ---- QRCode ----
-# qr = qrcode.QRCode()
-# qr.add_data("http://192.168.0.107:8000/client/index.html")
-# f = io.StringIO()
-# qr.print_ascii(out=f)
-# f.seek(0)
-# print(f.read())
+qr = qrcode.QRCode()
+qr.add_data("http://192.168.0.107:8000/client/controller.html")
+f = io.StringIO()
+qr.print_ascii(out=f)
+f.seek(0)
+print(f.read())
 
 
 @app.get("/")
@@ -38,16 +38,17 @@ def root():
 #     return {"event": f"{button_id} pressed"}
 
 
-# @app.websocket("/ws/gamepad")
-# async def handle_press_ws(websocket: WebSocket):
-#     await websocket.accept()
-#     try:
-#         while True:
-#             button = await websocket.receive_text()
-#             # keyboard.press(button)
-#             time.sleep(0.25)
-#             # keyboard.release(button)
-#             await websocket.send_text(f"{button} was pressed")
-#     except WebSocketDisconnect:
-#         await websocket.close()
-#         print("client disconnected")
+@app.websocket("/ws/gamepad")
+async def handle_press_ws(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            button = await websocket.receive_text()
+            keyboard.press(button)
+            time.sleep(0.25)
+            keyboard.release(button)
+            await websocket.send_text(f"{button} was pressed")
+            print(f"{button} was pressed")
+    except WebSocketDisconnect:
+        await websocket.close()
+        print("client disconnected")

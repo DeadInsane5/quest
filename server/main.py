928bd6d35fe5d93ba364088faf1e40ba659890be
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2025-11-26 17:31:59 +0530

    [v1]

--- server/main.py
+++ server/main.py
@@ -2,18 +2,29 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
+from pydantic import BaseModel
+
 from pynput.keyboard import Controller
 import time
 import io
 import qrcode
+
+
+# ---- Player Class ----
+class Player(BaseModel):
+    name: str
+    energy: int | None = None
+    stress: int | None = None
+    happiness: int | None = None
+
 
 keyboard = Controller()
 
 app = FastAPI()
 app.mount("/client", StaticFiles(directory="client"), name="client")
 
-# origins = ["http://localhost:8000"]
-origins = ["http://192.168.0.100:8000"]
+origins = ["http://localhost:8000"]
+# origins = ["http://192.168.0.100:8000"]
 app.add_middleware(CORSMiddleware, allow_origins=origins)
 
 # ---- QRCode ----
@@ -36,6 +47,43 @@ def root():
 #     # time.sleep(0.5)
 #     # keyboard.release(button_id)
 #     return {"event": f"{button_id} pressed"}
+
+
+@app.get("/api/profile")
+async def get_profile():
+    if current_player is None:
+        return {"error": "No profile created yet"}
+    return current_player
+
+
+@app.post("/api/profile/create_profile")
+async def create_profile(player: Player):
+    player.energy = 10
+    player.stress = 10
+    player.happiness = 10
+    global current_player
+    current_player = player
+    print(player)
+    return player
+
+
+@app.patch("/api/profile")
+async def update_profile(updates: dict):
+    global current_player
+
+    if current_player is None:
+        return {"error": "No profile exists"}
+
+    if "energy" in updates:
+        current_player.energy = updates["energy"]
+    if "stress" in updates and current_player.stress > 0:
+        current_player.stress = updates["stress"]
+    if "happiness" in updates:
+        current_player.happiness = updates["happiness"]
+    if "name" in updates:
+        current_player.name = updates["name"]
+
+    return current_player
 
 
 @app.websocket("/ws/gamepad")
